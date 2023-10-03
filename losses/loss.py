3db4d7dddc9c9641ebacf4319bdd208bc5295b07
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-10-03 23:47:03 +0300

    make loss callable

--- losses/loss.py
+++ losses/loss.py
@@ -1,2 +1,5 @@
 class Loss:
-    pass
+
+    @staticmethod
+    def __call__(*args, **kwargs):
+        raise NotImplementedError("Loss function is not implemented. You should implement it in subclass.")
