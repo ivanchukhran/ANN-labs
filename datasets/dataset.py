6dd816f7c21df0e61550435d3501a34889a87d68
IhorRodionov <73175941+IhorRodionov@users.noreply.github.com>
2023-10-03 22:18:49 +0300

    Implement dataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,4 +1,9 @@
+from typing import List
+
 import pandas as pd
+import os
+from pathlib import Path
+import numpy as np
 
 
 class Dataset:
@@ -8,22 +13,49 @@ class Dataset:
     Args:
         root_dir (str): Root directory of the dataset.
         train (bool): If True, creates dataset from training set, otherwise creates from test set.
-        """
+        batch_size (int): Size of batch.
+        x_labels (List[str]): List of labels for x values.
+        y_labels (List[str]): List of labels for y values.
+    """
     root_dir: str
     train: bool
     batch_size: int
     data: pd.DataFrame
+    x_labels: List[str]
+    y_labels: List[str]
 
-    def __init__(self, root_dir: str, train: bool = True, batch_size: int = 1):
+    def __init__(self,
+                 root_dir: str,
+                 x_labels: List[str],
+                 y_labels: List[str],
+                 train: bool = True,
+                 batch_size: int = 1):
+
         self.root_dir = root_dir
         self.train = train
         self.batch_size = batch_size
-        # TODO: Implement data loading from CSV or other format.
-        # TODO: Implement batch loading.
+        self.y_labels = y_labels
+        self.x_labels = x_labels
+
+        if not os.path.isfile(self.root_dir):
+            raise FileNotFoundError("Dataset file not found.")
+
+        match Path(self.root_dir).suffix:
+            case ".csv":
+                self.data = pd.read_csv(self.root_dir)
+            case ".json":
+                self.data = pd.read_json(self.root_dir)
+            case ".xlsx":
+                self.data = pd.read_excel(self.root_dir)
+            case _:
+                raise NotImplementedError("Dataset file format not supported.")
 
     def __len__(self):
-        return len(self.data)
+        """Returns length of dataset."""
+        return len(self.data) - self.batch_size
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> (np.ndarray, np.ndarray):
         """Returns sample from dataset in format (x, y) where x is numpy.ndarray, and y is scalar or ndarray too."""
-        raise NotImplementedError("You should implement this method in subclass.")
+        x = self.data[self.x_labels].iloc[idx:idx + self.batch_size]
+        y = self.data[self.y_labels].iloc[idx:idx + self.batch_size]
+        return x, y
