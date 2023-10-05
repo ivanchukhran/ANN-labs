6179351f9dd5da656121f01bb40da28ac9ce20eb
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-10-05 20:14:27 +0300

    change module members

--- modules/linear.py
+++ modules/linear.py
@@ -9,23 +9,17 @@ class Linear(Module):
         super(Linear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = np.random.randn(in_features, out_features)
-        self.bias = np.random.randn(out_features)
-        self.grad_weight = np.zeros_like(self.weight)
-        self.grad_bias = np.zeros_like(self.bias)
+        weight = np.random.randn(in_features, out_features)
+        bias = np.random.randn(out_features)
+        self.__parameters = [weight, bias]
+        grad_weight = np.zeros_like(weight)
+        grad_bias = np.zeros_like(bias)
+        self.__grads = [grad_weight, grad_bias]
 
     def forward(self, x):
-        return np.dot(x, self.weight) + self.bias
+        return np.dot(x, self.__parameters[0]) + self.__parameters[1]
 
     def backward(self, x: np.ndarray, grad_output: np.ndarray):
-        self.grad_weight = np.dot(x.T, grad_output)
-        self.grad_bias = np.sum(grad_output, axis=0)
-        return np.dot(grad_output, self.weight.T)
-
-    def parameters(self):
-        return [self.weight, self.bias]
-
-    def grad_parameters(self):
-        return [self.grad_weight, self.grad_bias]
-
-
+        self.__grads[0] = np.dot(x.T, grad_output)
+        self.__grads[1] = np.sum(grad_output, axis=0)
+        return np.dot(grad_output, self.__parameters[0].T)
--- modules/module.py
+++ modules/module.py
@@ -2,6 +2,8 @@ import abc
 
 
 class Module(abc.ABC):
+    __parameters: list = None
+    __grads: list = None
     """
     Abstract class for all neural network modules.
     Your models should also inherit from this class.
@@ -14,13 +16,13 @@ class Module(abc.ABC):
         raise NotImplementedError("Backward pass is not implemented. You should implement it in subclass.")
 
     def parameters(self):
-        raise NotImplementedError("Parameters getter is not implemented. You should implement it in subclass.")
+        return self.__parameters
 
     def has_parameters(self):
-        return self.parameters() is not None
+        return self.__parameters is not None
 
-    def grad_parameters(self):
-        raise NotImplementedError("Gradients getter is not implemented. You should implement it in subclass.")
+    def grads(self):
+        return self.__grads
 
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
@@ -30,3 +32,14 @@ class Module(abc.ABC):
 
     def update_parameters(self):
         raise NotImplementedError("Parameters update is not implemented. You should implement it in subclass.")
+
+    def to_dict(self) -> dict:
+        return {self.__class__.__name__: self.parameters()}
+
+    def from_dict(self, config: dict):
+        try:
+            self.__parameters = config[self.__class__.__name__]
+        except KeyError:
+            raise KeyError(f"Module {self.__class__.__name__} not found in config. "
+                           f"The expected module is {self.__class__.__name__} but got {config.keys()[0]} instead.")
+
