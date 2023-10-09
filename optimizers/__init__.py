d13bd871c534068593b9c73b944fcdc58d26aa95
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-10-09 13:07:06 +0300

    add Adagrad import

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -2,5 +2,6 @@ from .optimizer import Optimizer
 from .sgd import SGD
 from .adam import Adam
 from .rmsprop import RMSProp
+from .adagrad import Adagrad
 
-__all__ = ['Optimizer', 'SGD', 'Adam', 'RMSProp']
+__all__ = ['Optimizer', 'SGD', 'Adam', 'RMSProp', 'Adagrad']
