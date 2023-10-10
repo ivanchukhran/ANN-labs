0a22ef54b11f07e73ba1a5cb61e67e47cca5c652
IhorRodionov <73175941+IhorRodionov@users.noreply.github.com>
2023-10-10 14:10:33 +0300

    Functions

---
+++ functions/Sub.py
@@ -0,0 +1,20 @@
+from functools import reduce
+
+from tensor import Tensor
+from .function import Function
+
+
+class Sub(Function):
+    def __init__(self, saved_tensors: list):
+        super().__init__(saved_tensors)
+
+    def forward(self):
+        return reduce(lambda x, y: x - y, self.saved_tensors)
+
+    def backward(self, grad_output: Tensor = None):
+        for t in self.saved_tensors:
+            if t.requires_grad:
+                t.grad += grad_output.grad
+
+    def __call__(self, *args, **kwargs):
+        return self.forward()
