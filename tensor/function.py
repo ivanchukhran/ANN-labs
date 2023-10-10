62bf40a27367ee36c2b6bf8eb985e40ce97324d3
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-10-10 11:41:30 +0300

    minimal tensor and mini autograd interface

---
+++ tensor/function.py
@@ -0,0 +1,24 @@
+from .tensor import Tensor
+
+
+class Function:
+    """
+    Represent each tensor operation as a Function object.
+
+    Each Function object has a forward method and a backward method.
+    The forward method computes the output tensor given the input tensor.
+    The backward method computes the gradient of the loss with respect to the input tensor/s.
+
+    """
+
+    def __init__(self, saved_tensors: Tensor | list[Tensor] = None):
+        self.saved_tensors = saved_tensors
+        self.next_functions = None
+
+    def forward(self, *args):
+        raise NotImplementedError("Forward not implemented. "
+                                  "You should implement forward method in your Function subclass")
+
+    def backward(self, *grad_output):
+        raise NotImplementedError("Backward not implemented. "
+                                  "You should implement backward method in your Function subclass")
---
+++ tensor/tensor.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+_convertable = (int, float, list)
+
+
+class Tensor:
+    data: np.ndarray
+    requires_grad: bool
+    grad: np.ndarray
+    grad_fn: 'Function'
+
+    def __init__(self, data: np.ndarray | int | float | list, requires_grad: bool = False, grad_fn: 'Function' = None):
+        if isinstance(data, _convertable):
+            data = np.array(data)
+        if not isinstance(data, np.ndarray):
+            raise TypeError(f"unsupported operand type(s) for +: 'Tensor' and '{type(data)}'")
+        self.data = data
+        self.requires_grad = requires_grad
+        self.grad = np.zeros_like(data) if requires_grad else None
+        self.grad_fn = grad_fn
+
+    def __repr__(self):
+        return f"Tensor({self.data}, requires_grad={self.requires_grad})"
+
+    def __add__(self, other):
+        if isinstance(other, (_convertable, np.ndarray)):
+            other = Tensor(other)
+        if not isinstance(other, Tensor):
+            raise TypeError(f"unsupported operand type(s) for +: 'Tensor' and '{type(other)}'")
+        return Tensor(self.data + other.data, requires_grad=(self.requires_grad or other.requires_grad))
+
+    def __radd__(self, other):
+        if isinstance(other, (_convertable, np.ndarray)):
+            other = Tensor(other)
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        if isinstance(other, (_convertable, np.ndarray)):
+            other = Tensor(other)
+        if not isinstance(other, Tensor):
+            raise TypeError(f"unsupported operand type(s) for -: 'Tensor' and '{type(other)}'")
+        return Tensor(self.data - other.data, requires_grad=(self.requires_grad or other.requires_grad))
+
+    def __rsub__(self, other):
+        if isinstance(other, (int, float, np.ndarray)):
+            other = Tensor(other)
+        return other.__sub__(self)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float, np.ndarray)):
+            other = Tensor(other)
+        if not isinstance(other, Tensor):
+            raise TypeError(f"unsupported operand type(s) for *: 'Tensor' and '{type(other)}'")
+        return Tensor(self.data * other.data, requires_grad=(self.requires_grad or other.requires_grad))
+
+    def __rmul__(self, other):
+        if isinstance(other, (int, float, np.ndarray)):
+            other = Tensor(other)
+        return self.__mul__(other)
+
+    def __neg__(self):
+        return self.__mul__(-1)
+
+    def __matmul__(self, other):
+        if isinstance(other, (int, float, np.ndarray)):
+            other = Tensor(other)
+        if not isinstance(other, Tensor):
+            raise TypeError(f"unsupported operand type(s) for @: 'Tensor' and '{type(other)}'")
+        return Tensor(self.data @ other.data, requires_grad=(self.requires_grad or other.requires_grad))
+
+    def __rmatmul__(self, other):
+        if isinstance(other, (int, float, np.ndarray)):
+            other = Tensor(other)
+        return other.__matmul__(self)
+
+    T = property(lambda self: self.transpose())
+
+    def transpose(self):
+        return Tensor(self.data.T, self.requires_grad)
+
