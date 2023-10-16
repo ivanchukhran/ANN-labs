d1065eaafb63167c310fb2e8cd5f8fdfa1742fc7
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-10-16 10:41:54 +0300

    reimplement nn module

--- modules/linear.py
+++ modules/linear.py
@@ -1,34 +1,26 @@
 import numpy as np
 from .module import Module
+from tensor import Parameter, Tensor
 
 
 class Linear(Module):
     """Linear layer of neural network."""
 
-    def __init__(self, in_features, out_features):
+    def __init__(self, in_features: int, out_features: int, use_bias: bool = True):
+        super(Linear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = np.random.randn(out_features, in_features)
-        self.bias = np.random.randn(out_features)
-        self._parameters = [self.weight, self.bias]
-        grad_weight = np.zeros_like(self.weight)
-        grad_bias = np.zeros_like(self.bias)
-        self._grads = [grad_weight, grad_bias]
-        super(Linear, self).__init__()
+        self.weight = Parameter(out_features, in_features)
+        self.use_bias = use_bias
+        if self.use_bias:
+            self.bias = Parameter(out_features)
 
-    def forward(self, x):
-        return np.dot(x, self.weight.T) + self.bias
+    def forward(self, x: Tensor):
+        output = x @ self.weight.T
+        if self.use_bias:
+            output += self.bias
+        return output
 
-    def backward(self, x: np.ndarray, grad_output: np.ndarray = None):
-        # if grad_output is None:
-        #     grad_output = 1.0
-        # self._grads[0] = np.dot(x, grad_output)
-        # self._grads[1] = np.sum(grad_output, axis=0)
-        # print(self._grads[0].shape, self._grads[1].shape)
-        # return np.dot(grad_output, self._grads[0])
+    def __repr__(self):
+        return f"Linear(in_features={self.in_features}, out_features={self.out_features}, use_bias={self.use_bias})"
 
-        if grad_output is None:
-            grad_output = np.ones_like(x)
-        self._grads[0] += np.dot(x.T, grad_output)
-        self._grads[1] += np.sum(grad_output, axis=0)
-        return np.dot(grad_output, self._parameters[0])
--- modules/module.py
+++ modules/module.py
@@ -1,38 +1,59 @@
-import abc
+from collections import OrderedDict
+from typing import Iterator
+
+from tensor import Parameter
 
 
-class Module(abc.ABC):
-    _parameters: list
-    _grads: list
+class Module:
     """
     Abstract class for all neural network modules.
     Your models should also inherit from this class.
     """
+    _training: bool
+    _parameters: OrderedDict
+    _modules: OrderedDict
 
-    # def __init__(self, *args, **kwargs):
-        # self._parameters = []
-        # self._grads = []
+    def __init__(self, *args, **kwargs):
+        super.__setattr__(self, '_parameters', OrderedDict())
+        super.__setattr__(self, '_modules', OrderedDict())
+        super.__setattr__(self, '_training', True)
+
+    def __setattr__(self, key, value):
+        if isinstance(value, Parameter):
+            self.register_parameter(key, value)
+        elif isinstance(value, Module):
+            self.register_module(key, value)
+        super.__setattr__(self, key, value)
+
+    def register_parameter(self, name: str, param: Parameter):
+        if name in self._parameters:
+            raise KeyError(f"Parameter {name} already exists.")
+        self._parameters[name] = param
+
+    def register_module(self, name: str, module: 'Module'):
+        if name in self._modules:
+            raise KeyError(f"Module {name} already exists.")
+        self._modules[name] = module
 
     def forward(self, *args, **kwargs):
         raise NotImplementedError("Forward pass is not implemented. You should implement it in subclass.")
 
-    def backward(self, *args, **kwargs):
-        raise NotImplementedError("Backward pass is not implemented. You should implement it in subclass.")
-
-    def parameters(self):
-        return self._parameters
+    def parameters(self) -> Iterator[Parameter]:
+        for k, v in self._parameters.items():
+            yield v
 
     def has_parameters(self):
-        return self._parameters is not None
+        return bool(self._parameters)
 
-    def grads(self):
-        return self._grads
+    def grads(self) -> Iterator[Parameter]:
+        for p in self.parameters():
+            yield p.grad
 
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({list(map(len, self.parameters()))})'
+        return f'{self.__class__.__name__}()'
 
     def update_parameters(self):
         raise NotImplementedError("Parameters update is not implemented. You should implement it in subclass.")
