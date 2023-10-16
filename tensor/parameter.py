afba9114143ff35a7787dba7239c6b3d078d8469
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-10-16 09:44:53 +0300

    initial parameter implementation

---
+++ tensor/parameter.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from ._tensor import Tensor
+
+
+class Parameter(Tensor):
+    def __init__(self, *features):
+        self.data = np.random.randn(*features)
+        super().__init__(self.data, requires_grad=True)
+
+    def __repr__(self):
+        return f"Parameter({self.data.shape})"
+
+
